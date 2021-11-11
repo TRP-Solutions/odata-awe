ed284a65b3a9f37a8ecd4c7bae7d7e4e578c9c1d
Jacob Neergaard Pedersen <neergaard@kananga.dk>
2021-11-11 15:44:26 +0100

    fix count SQL

--- lib/ODataAweMySQL.php
+++ lib/ODataAweMySQL.php
@@ -73,9 +73,10 @@ class ODataAweMySQL extends ODataAwe {
 		$offset = $odata->getSkip() ? 'OFFSET '.$odata->getSkip() : "";
 
 		// Set Count
-		$sql = "SELECT COUNT(*) as count
-			FROM $setup[database]
-			WHERE $where";
+		$sql = "SELECT COUNT(*) as count ";
+		$sql .= "FROM $setup[database] ";
+		if($setup['group']) $sql .= "GROUP BY ".implode(',',$setup['group']).' ';
+		$sql .= "WHERE $where ";
 		$query = $this->dbconn->query($sql);
 		$odata->setCount($query->fetch_assoc()['count']);
 
